c6569b03987d3cb13ec9cd717e624ade9c0462a5
BrianMcMurr <bmcmurray@pugetsound.edu>
2023-04-24 22:23:57 +0000

    test commit

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "vector.h"
 #include "banker.h"
-
+//Testing to see if I can push
 int main(int argc, char *argv[])
 {
   // TODO: attempt to open scenario file and scan data into allocated structures
